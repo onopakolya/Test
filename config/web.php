c72db874bd62276b135fe92fc3e71115ba763cdb
Nikolay <onopakolya@gmail.com>
2022-08-23 22:31:56 +0500

    Добавили модуль Админ

--- config/web.php
+++ config/web.php
@@ -21,8 +21,18 @@ $config = [
         'admin' => [
             'class' => 'app\module\admin\Module',
         ],
+        'adminka' => [
+            'class' => 'mdm\admin\Module',
+        ],
+        'test' => [
+            'class' => 'app\module\test\Module',
+        ],
     ],
     'components' => [
+
+    'authManager' => [
+            'class' => 'yii\rbac\PhpManager', 
+    ], // or use 'yii\rbac\DbManager'
     'request' => [
         // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
         'cookieValidationKey' => 'f2kmjfs7dLoGWqH07xfO8ydRlqhWrakR',
@@ -60,6 +70,20 @@ $config = [
         'rules' => [
             ],
         ],
+    ],
+
+    'as access' => [
+        'class' => 'mdm\admin\components\AccessControl',
+        'allowActions' => [
+            'site/*',
+            'admin/*',
+            'adminka/*',
+            // The actions listed here will be allowed to everyone including guests.
+            // So, 'admin/*' should not appear here in the production, of course.
+            // But in the earlier stages of your development, you may probably want to
+            // add a lot of actions here until you finally completed setting up rbac,
+            // otherwise you may not even take a first step.
+        ]
     ],
     'params' => $params,
 ];
